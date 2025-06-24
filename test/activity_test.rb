1f461a8e80f89356fcb5f01dc9c0f180348b00ec
Tom Wheeler <tom@temporal.io>
2025-06-23 19:08:50 -0500

    add test for withdraw Activity

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -13,6 +13,13 @@ module MoneyTransfer
       result = env.run(BankActivities::Withdraw.new, details)
       assert_equal('OKW-100-A1001', result)
     end
+
+    def test_deposit
+      env = Temporalio::Testing::ActivityEnvironment.new
+      details = MoneyTransfer::TransferDetails.new('A1001', 'B2002', 100, 'REF123')
+      result = env.run(BankActivities::Deposit.new, details)
+      assert_equal('OKD-100-B2002', result)
+    end
   end
 end
 

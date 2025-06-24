6252c9ff2d32a64bc95b193a014cbfcd2c517cfb
Tom Wheeler <tom@temporal.io>
2025-06-23 19:45:03 -0500

    Verify withdrawal failure with insufficient funds

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -14,6 +14,15 @@ module MoneyTransfer
       assert_equal('OKW-100-A1001', result)
     end
 
+    def test_withdraw_fails_insufficient_funds
+      env = Temporalio::Testing::ActivityEnvironment.new
+      details = MoneyTransfer::TransferDetails.new('A1001', 'B2002', 500_000, 'REF123')
+      err = assert_raises(MoneyTransfer::InsufficientFundsError) do
+        env.run(BankActivities::Withdraw.new, details)
+      end
+      assert_equal('Transfer amount too large', err.message)
+    end
+
     def test_deposit
       env = Temporalio::Testing::ActivityEnvironment.new
       details = MoneyTransfer::TransferDetails.new('A1001', 'B2002', 100, 'REF123')

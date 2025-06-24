ffcf95061aca228a01948a34a5d2eedbe2dcd063
Tom Wheeler <tom@temporal.io>
2025-06-23 19:11:11 -0500

    Define custom types for error handling

--- shared.rb
+++ shared.rb
@@ -5,6 +5,14 @@ require 'json/add/struct'
 module MoneyTransfer
   TASK_QUEUE_NAME = 'money-transfer'
 
+  # InsufficientFundsError is raised when the source account
+  # balance is too low to successfully complete the withdrawal.
+  class InsufficientFundsError < StandardError; end
+
+  # InvalidAccountError is raised when the account identifier
+  # for the transaction does not reference an active account.
+  class InvalidAccountError < StandardError; end
+
   # TransferDetails is the input to MoneyTransferWorkflow. It specifies
   # the source and target accounts, the amount of the transfer, and a
   # reference ID that uniquely identifies the transaction.

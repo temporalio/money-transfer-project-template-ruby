587ce4dcf0ad0aff6cf94550fb03b4de4ec65c2a
Tom Wheeler <tom@temporal.io>
2025-06-23 19:43:27 -0500

    Add custom retry policy with non-retryable type

--- workflow.rb
+++ workflow.rb
@@ -2,6 +2,7 @@
 
 require_relative 'activities'
 require_relative 'shared'
+require 'temporalio/retry_policy'
 require 'temporalio/workflow'
 
 module MoneyTransfer
@@ -9,19 +10,28 @@ module MoneyTransfer
   # account and deposits it into the target account.
   class MoneyTransferWorkflow < Temporalio::Workflow::Definition
     def execute(details)
+      retry_policy = Temporalio::RetryPolicy.new(
+        max_attempts: 3,
+        max_interval: 2,
+        non_retryable_error_types: [
+          'InsufficientFundsError'
+        ]
+      )
       Temporalio::Workflow.logger.info("Starting workflow (#{details})")
 
       withdraw_result = Temporalio::Workflow.execute_activity(
         BankActivities::Withdraw,
         details,
-        start_to_close_timeout: 5
+        start_to_close_timeout: 5,
+        retry_policy: retry_policy
       )
       Temporalio::Workflow.logger.info("Withdrawal confirmation: #{withdraw_result}")
 
       deposit_result = Temporalio::Workflow.execute_activity(
         BankActivities::Deposit,
         details,
-        start_to_close_timeout: 5
+        start_to_close_timeout: 5,
+        retry_policy: retry_policy
       )
       Temporalio::Workflow.logger.info("Deposit confirmation: #{deposit_result}")
 

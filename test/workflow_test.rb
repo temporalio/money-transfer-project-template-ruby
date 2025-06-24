bd01b98e873fd7898be3e6d204dd686febecad7c
Tom Wheeler <tom@temporal.io>
2025-06-23 19:43:38 -0500

    Verify that insufficient funds fails the Workflow Execution

--- test/workflow_test.rb
+++ test/workflow_test.rb
@@ -9,24 +9,53 @@ require_relative '../workflow'
 
 module MoneyTransfer
   class MoneyTransferWorkflowTest < Minitest::Test
+    def setup
+      @@env ||= Temporalio::Testing::WorkflowEnvironment.start_local
+    end
+
+    Minitest.after_run do
+      @@env&.shutdown
+    end
+
     def test_workflow
-      Temporalio::Testing::WorkflowEnvironment.start_local do |env|
-        worker = Temporalio::Worker.new(
-          client: env.client,
-          task_queue: "#{TASK_QUEUE_NAME}-test",
-          activities: [BankActivities::Withdraw, BankActivities::Deposit],
-          workflows: [MoneyTransferWorkflow]
+      worker = create_worker([BankActivities::Withdraw, BankActivities::Deposit])
+
+      worker.run do
+        assert_equal(
+          'Transfer complete (transaction IDs: OKW-100-A1001, OKD-100-B2002)',
+          @@env.client.execute_workflow(
+            MoneyTransferWorkflow,
+            MoneyTransfer::TransferDetails.new('A1001', 'B2002', 100, 'REF123'),
+            id: "wf-#{SecureRandom.uuid}",
+            task_queue: worker.task_queue
+          )
         )
-        worker.run do
-          assert_equal(
-            'Transfer complete (transaction IDs: OKW-100-A1001, OKD-100-B2002)',
-            env.client.execute_workflow(MoneyTransferWorkflow,
-                                        MoneyTransfer::TransferDetails.new('A1001', 'B2002', 100, 'REF123'),
-                                        id: "wf-#{SecureRandom.uuid}",
-                                        task_queue: worker.task_queue)
+      end
+    end
+
+    def test_workflow_fails_if_insufficient_funds
+      worker = create_worker([BankActivities::Withdraw, BankActivities::Deposit])
+
+      worker.run do
+        assert_raises(Temporalio::Error::WorkflowFailedError) do
+          @@env.client.execute_workflow(
+            MoneyTransferWorkflow,
+            MoneyTransfer::TransferDetails.new('A1001', 'B2002', 500_000, 'REF123'),
+            id: "wf-#{SecureRandom.uuid}",
+            task_queue: worker.task_queue
           )
         end
       end
     end
+
+    def create_worker(activities)
+      Temporalio::Worker.new(
+        client: @@env.client,
+        task_queue: "#{TASK_QUEUE_NAME}-test",
+        activities: activities,
+        workflows: [MoneyTransferWorkflow]
+      )
+    end
   end
 end
+

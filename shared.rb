227dfae9d1d3beaca85a545b36a952c7fa126078
Tom Wheeler <tom@temporal.io>
2025-07-03 17:14:02 -0500

    Changed definition of custom error types as a temporary workaround for a Ruby SDK bug (#294)

--- shared.rb
+++ shared.rb
@@ -1,17 +1,30 @@
 # frozen_string_literal: true
 
 require 'json/add/struct'
+require 'temporalio/error'
 
 module MoneyTransfer
   TASK_QUEUE_NAME = 'money-transfer'
 
-  # InsufficientFundsError is raised when the source account
-  # balance is too low to successfully complete the withdrawal.
-  class InsufficientFundsError < StandardError; end
+  # NOTE: The two custom error types that follow are defined in an
+  # unusual way, by subclassing ApplicationError and overriding its
+  # non_retryable method. This is a workaround for an SDK bug (#294).
+  # Typically, you would subclass StandardError and specify the type
+  # as non-retryable in the RetryPolicy, but this approach does not
+  # work due to the bug. We'll update the code once the bug is fixed.
+  class InsufficientFundsError < Temporalio::Error::ApplicationError
+    def non_retryable
+      true
+    end
+  end
 
   # InvalidAccountError is raised when the account identifier
   # for the transaction does not reference an active account.
-  class InvalidAccountError < StandardError; end
+  class InvalidAccountError < Temporalio::Error::ApplicationError
+    def non_retryable
+      true
+    end
+  end
 
   # TransferDetails is the input to MoneyTransferWorkflow. It specifies
   # the source and target accounts, the amount of the transfer, and a
